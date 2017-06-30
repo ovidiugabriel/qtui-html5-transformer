f320f98329f8ac76b3d75c07d277be0c69555f69
Ovidiu <ovidiugabriel@gmail.com>
2017-06-30 15:58:09 +0300

    Update QPushButton.php

--- QPushButton.php
+++ QPushButton.php
@@ -1,13 +1,28 @@
 <?php
 
+/**
+ * Used to convert a QPushButton into a <input> HTML tag with type="button"
+ * https://www.w3schools.com/tags/tag_input.asp
+ */
 class QPushButton {
     private $name;
     private $value;
-    public function __construct($name, $properties) {
+    
+    /** 
+     * @param string $name
+     * @param array $properties
+     */
+    public function __construct($name, array $properties) {
         $this->name = $name;
         $this->value = $properties[1]->string;
     }
+    
+    /** 
+     * @return string
+     */
     public function __toString() {
         return '<input type="button" id="'.$this->name.'" name="'.$this->name.'" value="'.$this->value.'" />' . "\n";
     }
 }
+
+// EOF
