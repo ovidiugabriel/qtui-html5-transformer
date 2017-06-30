8a05c70cf5a200e7fe88cd0df810e3429e9fe446
Ovidiu <ovidiugabriel@gmail.com>
2017-06-30 15:55:49 +0300

    Update QPushButton.php

--- QPushButton.php
+++ QPushButton.php
@@ -1,1 +1,13 @@
+<?php
 
+class QPushButton {
+    private $name;
+    private $value;
+    public function __construct($name, $properties) {
+        $this->name = $name;
+        $this->value = $properties[1]->string;
+    }
+    public function __toString() {
+        return '<input type="button" id="'.$this->name.'" name="'.$this->name.'" value="'.$this->value.'" />' . "\n";
+    }
+}
