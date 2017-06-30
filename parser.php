f76cbcf44b941b40f5fbc911e3a328e56bc102ec
Ovidiu <ovidiugabriel@gmail.com>
2017-06-30 15:56:16 +0300

    Update parser.php

--- parser.php
+++ parser.php
@@ -1,18 +1,6 @@
 <?php
 
-class QPushButton {
-    private $name;
-    private $value;
-
-    public function __construct($name, $properties) {
-        $this->name = $name;
-        $this->value = $properties[1]->string;
-    }
-
-    public function __toString() {
-        return '<input type="button" id="'.$this->name.'" name="'.$this->name.'" value="'.$this->value.'" />' . "\n";
-    }
-}
+require_once 'QPushButton.php';
 
 $xml = simplexml_load_file('dialog.ui');  // my test file for now
 
